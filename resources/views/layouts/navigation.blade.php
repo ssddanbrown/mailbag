bd2cd76864f138f89bfddb4eff3c715c802cb49b
Dan Brown <ssddanbrown@googlemail.com>
2020-12-20 01:51:34 +0000

    Added little custom log

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        <x-application-logo class="block h-8 w-auto fill-current text-indigo-600" />
                     </a>
                 </div>
 
