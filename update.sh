60868f1f0cde22027e8600c5c491d433e5ba6c8e
Dan Brown <ssddanbrown@googlemail.com>
2020-12-24 23:49:51 +0000

    Added db backup and queue restart to update script

--- update.sh
+++ update.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# This should be executed from the top-level mailbag install folder.
+# (Where this script is by default)
+
 php artisan down && \
+cp storage/database/database.sqlite ./database-backup.sqlite
 git pull origin master && \
 composer install --optimize-autoloader --no-dev && \
 php artisan migrate && \
 npm install && \
 npm run build && \
+php artisan queue:restart && \
 php artisan config:cache && \
 php artisan route:cache && \
 php artisan view:cache && \
