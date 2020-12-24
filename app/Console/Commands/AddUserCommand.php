95a1cbeaa60fc395cc30db6a62fda6af81c25d88
Dan Brown <ssddanbrown@googlemail.com>
2020-12-24 00:55:44 +0000

    Added command to create new users

---
+++ app/Console/Commands/AddUserCommand.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class AddUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mailbag:add-user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Add a new login user to the system';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $name = $this->ask("Please provide a name for the user");
+        $email = $this->ask("Please provide an email address for the user");
+        $password = $this->secret("Now please provide a password");
+
+        foreach (compact('name', 'email', 'password') as $prop => $value) {
+            if (empty($value)) {
+                $this->error("Provided {$prop} value is empty");
+                return 1;
+            }
+        }
+
+        $user = User::query()->where('email', '=', $email)->first();
+        if (!is_null($user)) {
+            $update = $this->confirm("User with that email already exists, Do you want to update them?");
+            if (!$update) {
+                $this->error("Taking no action");
+                return 1;
+            }
+        } else {
+            $user = new User();
+        }
+
+        $user->fill([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+        $user->save();
+
+        $this->info("User created, You can now login with the email {$email} and your provided password.");
+        return 0;
+    }
+}
---
+++ tests/Feature/AddUserCommandTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class AddUserCommandTest extends TestCase
+{
+
+    public function test_create_new_user()
+    {
+        $this->artisan('mailbag:add-user')
+            ->expectsQuestion('Please provide a name for the user', 'Barry')
+            ->expectsQuestion('Please provide an email address for the user', 'barry@example.com')
+            ->expectsQuestion('Now please provide a password', 'testpass')
+            ->expectsOutput('User created, You can now login with the email barry@example.com and your provided password.');
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'Barry',
+            'email' => 'barry@example.com'
+        ]);
+
+        $this->assertTrue(Auth::attempt(['email' => 'barry@example.com', 'password' => 'testpass']));
+    }
+
+    public function test_create_new_user_with_empty_detail_returns_error()
+    {
+        $this->artisan('mailbag:add-user')
+            ->expectsQuestion('Please provide a name for the user', 'Barry')
+            ->expectsQuestion('Please provide an email address for the user', 'barry@example.com')
+            ->expectsQuestion('Now please provide a password', '')
+            ->expectsOutput('Provided password value is empty')
+            ->assertExitCode(1);
+    }
+
+    public function test_command_when_email_used()
+    {
+        $user = User::factory()->create(['email' => 'barry@example.com']);
+        $this->artisan('mailbag:add-user')
+            ->expectsQuestion('Please provide a name for the user', 'Barry 123')
+            ->expectsQuestion('Please provide an email address for the user', 'barry@example.com')
+            ->expectsQuestion('Now please provide a password', 'testpass')
+            ->expectsConfirmation('User with that email already exists, Do you want to update them?', 'yes')
+            ->expectsOutput('User created, You can now login with the email barry@example.com and your provided password.');
+
+        $this->assertDatabaseHas('users', [
+            'id' => $user->id,
+            'name' => 'Barry 123',
+            'email' => 'barry@example.com'
+        ]);
+    }
+
+    public function test_command_when_email_used_but_reject_update()
+    {
+        $user = User::factory()->create(['email' => 'barry@example.com']);
+        $this->artisan('mailbag:add-user')
+            ->expectsQuestion('Please provide a name for the user', 'Barry 123')
+            ->expectsQuestion('Please provide an email address for the user', 'barry@example.com')
+            ->expectsQuestion('Now please provide a password', 'testpass')
+            ->expectsConfirmation('User with that email already exists, Do you want to update them?', 'no')
+            ->expectsOutput('Taking no action')
+            ->assertExitCode(1);
+
+        $this->assertDatabaseMissing('users', [
+            'id' => $user->id,
+            'name' => 'Barry 123',
+            'email' => 'barry@example.com'
+        ]);
+    }
+}
