5b034cf5992da9d68790121f6d7c5ce6fc0a6a7c
Dan Brown <ssddanbrown@googlemail.com>
2020-12-24 22:31:56 +0000

    Addded testing for homepage redirections

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+
+    public function test_root_url_visit_when_logged_in_redirects_to_dash()
+    {
+        $response = $this->whileLoggedIn()->get('/');
+        $response->assertRedirect(route('dashboard'));
+    }
+
+    public function test_root_url_visit_when_not_logged_in_redirects_to_login()
+    {
+        $response = $this->get('/');
+        $response->assertRedirect(route('login'));
+    }
+
+    public function test_root_url_visit_when_not_logged_in_with_location_configured_redirects_to_chosen_url()
+    {
+        config()->set('app.home_redirect_url', 'https://example.com/donkey');
+        $response = $this->get('/');
+        $response->assertRedirect('https://example.com/donkey');
+    }
+}
