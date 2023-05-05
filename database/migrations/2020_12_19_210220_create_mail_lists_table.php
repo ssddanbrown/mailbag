6162348fbf77737ee627bbf4f771514fca595ba7
Shift <shift@laravelshift.com>
2023-05-05 15:50:57 +0000

    Adopt anonymous migrations

--- database/migrations/2020_12_19_210220_create_mail_lists_table.php
+++ database/migrations/2020_12_19_210220_create_mail_lists_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMailListsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -31,4 +31,4 @@ class CreateMailListsTable extends Migration
     {
         Schema::dropIfExists('mail_lists');
     }
-}
+};
