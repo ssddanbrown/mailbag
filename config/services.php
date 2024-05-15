8a0ce9baffe3b716276d2ef41b32c02168b43526
Dan Brown <ssddanbrown@googlemail.com>
2024-05-15 20:56:30 +0100

    Added tweaks and fixes post-shift

--- config/services.php
+++ config/services.php
@@ -2,17 +2,22 @@
 
 return [
 
-    'hcaptcha' => [
-        'active' => ! empty(env('HCAPTCHA_SITEKEY', null)),
-        'sitekey' => env('HCAPTCHA_SITEKEY', null),
-        'secretkey' => env('HCAPTCHA_SECRETKEY', null),
-    ],
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
 
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-        'scheme' => 'https',
+    'hcaptcha' => [
+        'active'    => !empty(env('HCAPTCHA_SITEKEY', null)),
+        'sitekey'   => env('HCAPTCHA_SITEKEY', null),
+        'secretkey' => env('HCAPTCHA_SECRETKEY', null),
     ],
 
 ];
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,11 +4,12 @@ namespace Tests;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use DatabaseMigrations;
+    use RefreshDatabase;
 
     protected function whileLoggedIn(): self
     {
