7ea83867d0aa07aae97dd250f9d8418a1fc11689
Shift <shift@laravelshift.com>
2024-05-15 19:25:55 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -9,19 +9,5 @@ return [
 
     'home_redirect_url' => env('HOME_REDIRECT_URL', null),
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
 ];
